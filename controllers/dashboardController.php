5a7ea48b8356c896f457401bfb87471ca491b4a8
afmurillo97 <afmurillo97@gmail.com>
2022-11-27 13:40:43 -0500

    add dashboard controller

---
+++ controllers/dashboardController.php
@@ -0,0 +1,52 @@
+<?php
+	try{
+		@session_start();
+		include_once 'permisosPlataforma.php';
+
+		function totalActivos() {
+			require 'conexion.php';
+
+			$sql=$con->prepare('SELECT COUNT(*) FROM cronograma WHERE fechaEliminacion IS NULL');
+			$resultado=$sql->execute();
+			$resultado=$sql->fetchAll();
+			$num=$sql->rowCount();
+
+			if ($num>=1) {
+				foreach ($resultado as $fila) {
+					return $fila['COUNT(*)'];
+				}
+			}else{
+				return NULL;
+			}
+			$con=null;
+		}
+
+		function getCronogramas($inicio, $fin) {
+			require 'conexion.php';
+
+			$sql=$con->prepare('SELECT cronograma.*, clientes.nombre AS cliente, equipos.marcas AS marca, equipos.modelos AS modelo FROM cronograma WHERE fechaEliminacion IS NULL LIMIT :P1,:P2');
+			$sql->bindParam(':P1', $inicio, PDO::PARAM_INT);
+			$sql->bindParam(':P2', $fin, PDO::PARAM_INT);
+			$resultado=$sql->execute();
+			$resultado=$sql->fetchAll();
+			$num=$sql->rowCount();
+
+			if ($num>=1) {
+				return $resultado;
+			}else{
+				return NULL;
+			}
+			$con=null;
+		}
+
+		require 'conexion.php';
+
+		if(isset($_POST['accion'])) {
+			
+		}
+		$con=null;	
+	}catch(PDOException $error){
+		echo "ERROR: ".$error->getMessage();
+		exit();
+	}
+?>
